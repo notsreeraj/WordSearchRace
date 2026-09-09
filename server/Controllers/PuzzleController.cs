c9944fddb5b4064169367effca7a80f8fc40159b
notsreeraj <109159574+notsreeraj@users.noreply.github.com>
2026-09-09 00:20:35 -0400

    scaffolding folders and files forserver side

---
+++ server/Controllers/PuzzleController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace server.Controllers
+{
+    [Route("[controller]")]
+    public class PuzzleController : Controller
+    {
+        private readonly ILogger<PuzzleController> _logger;
+
+        public PuzzleController(ILogger<PuzzleController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
